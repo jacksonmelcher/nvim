0937356a4be6da6d6849ee3f51d7a3180d34e683
jacksonm <jacksonm@getamplifylife.com>
2023-09-05 10:34:01 -0400

    added harpoon

--- mappings.lua
+++ mappings.lua
@@ -20,13 +20,19 @@ return {
     },
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
-    ["<leader>b"] = { name = "Buffers" },
+    ["<leader>b"] = { name = "BUNCKER" },
     -- quick save
     ["<C-s>"] = { ":w!<cr>", desc = "Save File" }, -- change description but the same command
     ["<C-h>"] = { "<cmd> TmuxNavigateLeft<cr>", desc = "window left" },
+    -- tmux navigator
     ["<C-l>"] = { "<cmd> TmuxNavigateRight<cr>", desc = "window rigt" },
     ["<C-j>"] = { "<cmd> TmuxNavigateDown<cr>", desc = "window down" },
     ["<C-k>"] = { "<cmd> TmuxNavigateUp<cr>", desc = "window up" },
+    -- harpoon
+    ["<leader>h"] = { name = "Harpoon" },
+    ["<leader>ha"] = { "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = "Harpoon Mark" },
+    ["<leader>hr"] = { "<cmd>lua require('harpoon.mark').rm_file()<cr>", desc = "Harpoon Remove" },
+    ["<leader>he"] = { "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = "Harpoon Toggle Quick Menu" },
   },
   t = {
     -- setting a mapping to false will disable it
---
+++ plugins/harpoon.lua
@@ -0,0 +1,6 @@
+return {
+  "ThePrimeagen/harpoon",
+  config = function()
+    -- Add your configuration for Vim Harpoon here
+  end,
+}
